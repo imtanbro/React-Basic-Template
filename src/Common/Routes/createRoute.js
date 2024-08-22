6560742a02b01dff03172406c2791a3a7a28a0dc
Tanmay Gangurde <tanmay.gangurde@gmail.com>
2024-08-23 03:41:08 +0530

    Lazy loading

--- src/Common/Routes/createRoute.js
+++ src/Common/Routes/createRoute.js
@@ -1,20 +1,65 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../../App";
-import LandingPage from "../../Pages/LandingPage";
-import AboutUs from "../../Pages/AboutUs";
-import ContactUs from "../../Pages/ContactUs";
 import { ABOUT_PAGE, CONTACT_US, HOME_PAGE } from "../Constants/routePaths";
-import ErrorPage from "../Components/ErrorPage";
+
+import { lazy, Suspense } from "react";
+import SectionHeading from "../Components/SectionHeading";
+const LandingPage = lazy(() => import("../../Pages/LandingPage"));
+const AboutUs = lazy(() => import("../../Pages/AboutUs"));
+const ContactUs = lazy(() => import("../../Pages/ContactUs"));
+const ErrorPage = lazy(() => import("../Components/ErrorPage"));
 
 export const appRoute = createBrowserRouter([
   {
     path: HOME_PAGE,
     element: <App />,
     children: [
-      { path: HOME_PAGE, element: <LandingPage /> },
-      { path: ABOUT_PAGE, element: <AboutUs /> },
-      { path: CONTACT_US, element: <ContactUs /> },
-      { path: "*", element: <ErrorPage /> },
+      {
+        path: HOME_PAGE,
+        element: <LandingPage />,
+      },
+      {
+        path: ABOUT_PAGE,
+        element: (
+          <Suspense
+            fallback={
+              <div className="flex flex-col justify-center items-center">
+                <SectionHeading heading={"Loading...."}></SectionHeading>
+              </div>
+            }
+          >
+            <AboutUs />
+          </Suspense>
+        ),
+      },
+      {
+        path: CONTACT_US,
+        element: (
+          <Suspense
+            fallback={
+              <div className="flex flex-col justify-center items-center">
+                <SectionHeading heading={"Loading...."}></SectionHeading>
+              </div>
+            }
+          >
+            <ContactUs />
+          </Suspense>
+        ),
+      },
+      {
+        path: "*",
+        element: (
+          <Suspense
+            fallback={
+              <div className="flex flex-col justify-center items-center">
+                <SectionHeading heading={"Loading...."}></SectionHeading>
+              </div>
+            }
+          >
+            <ErrorPage />
+          </Suspense>
+        ),
+      },
     ],
   },
 ]);
